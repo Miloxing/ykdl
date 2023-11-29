7794129f17d0ec594e0734fa7329b1b6164f4af4
zhangzongqian <z@qq.com>
2023-11-29 14:34:24 +0800

    edit

--- ykdl/util/download.py
+++ ykdl/util/download.py
@@ -22,6 +22,7 @@ def get_proxy():
         try:
             return requests.get("http://127.0.0.1:5010/get/").json().get("proxy")
         except:
+            return ''
             time.sleep(0.5)
 
 try:
